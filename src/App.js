e692fb3194677320a76bb47c626f055aa5cdcee4
IceD0G <34693762+IceD0G@users.noreply.github.com>
2020-10-18 14:58:25 +0300

    day1

---
+++ src/App.js
@@ -0,0 +1,64 @@
+
+import React from 'react';
+// import Calendar from './components/calendar/Calendar';
+// import Details from './components/details/Details';
+import Features from './components/features/Features';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Main from './components/main/Main';
+import FetchData from './service/FetchData';
+import './style.css'
+
+class App extends React.Component {
+
+  fetchData = new FetchData()
+
+  state = {
+    rocket: 'Falcon 1',
+    rocketFeatures: null,
+    rockets: [],
+    company: null,
+  }
+
+  componentDidMount() {
+    this.updateRocket()
+    this.updateCompany()
+  }
+
+  updateRocket() {
+    this.fetchData.getRocked()
+      .then(data => {
+        this.setState({ rockets: data.map(item => item.name) })
+        return data
+      })
+      .then(data => data.find(item => item.name === this.state.rocket))
+      .then(rocketFeatures => {
+        this.setState({ rocketFeatures })
+      })
+  }
+
+  updateCompany() {
+    this.fetchData.getCompany()
+      .then(company => this.setState({ company }))
+  }
+
+  changeRocket = rocket => {
+    this.setState({
+      rocket
+    }, this.updateRocket)
+  }
+
+  render() {
+    return (
+      <>
+        <Header rockets={this.state.rockets} changeRocket={this.changeRocket} />
+        <Main rocket={this.state.rocket} />
+        { this.state.rocketFeatures && <Features {... this.state.rocketFeatures} />}
+        { this.state.company && <Footer {...this.state.company.links} />}
+      </>
+    )
+  }
+
+}
+
+export default App;
